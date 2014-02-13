a1d877bbe266c7705bac65a20ee8006e1e84c2a4
BerkshireKnight <ian.knight.1990@gmail.com>
2014-02-13 15:02:20 +0000

    Actually added the new zombie. Derp.

---
+++ mcp/src/minecraft/org/educraft/number/NumberZombie.java
@@ -0,0 +1,53 @@
+package org.educraft.number;
+
+import java.util.Random;
+
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+
+import org.educraft.EduCraft;
+
+public class NumberZombie extends EntityZombie {
+	int droppedItemId, value;
+
+	public NumberZombie(World world) {
+		super(world);
+		setCustomNameTag("Number Zombie");
+		this.droppedItemId = Item.rottenFlesh.itemID;
+		this.value = (new Random().nextInt(10)) + 1;
+	}
+	
+	@Override
+	public boolean attackEntityFrom(DamageSource source, float damage) {
+		if (source.getDamageType().equals("dummy")) {
+			// dummy damage, set the dropped item to DummyCoin
+			this.droppedItemId = EduCraft.NUMBER.itemID;
+			return super.attackEntityFrom(source, damage);
+		} else if (source.getDamageType().equals("player")) {
+			// other damage caused by player, set dropped item to rotten flesh
+			this.droppedItemId = Item.rottenFlesh.itemID;
+			return super.attackEntityFrom(source, damage);
+		} else {
+			// miscellaneous damage, don't change the dropped item
+			return super.attackEntityFrom(source, damage);
+		}
+	}
+	
+	@Override
+	protected int getDropItemId() {
+		return this.droppedItemId;
+	}
+	
+	@Override
+	public EntityItem entityDropItem(ItemStack stack, float par2) {
+		if (stack.getItem() instanceof Numbers) {
+			stack.setItemDamage(value);
+		}
+		return super.entityDropItem(stack, par2);
+	}
+
+}
