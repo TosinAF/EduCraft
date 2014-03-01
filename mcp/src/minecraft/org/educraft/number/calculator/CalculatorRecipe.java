515e0b1a14f537701e9005889991b51b60c84d2a
Janos Bana <janosbana@gmail.com>
2014-03-01 01:04:04 +0000

    creates 1x3 input matrix with one output

--- mcp/src/minecraft/org/educraft/number/calculator/CalculatorRecipe.java
+++ mcp/src/minecraft/org/educraft/number/calculator/CalculatorRecipe.java
@@ -40,72 +40,22 @@ public class CalculatorRecipe implements IRecipe {
 		this.recipeOutput = par4ItemStack;
 	}
 	
-	/**
-	 * Used to check if a recipe matches current crafting inventory
-	 */
 	public boolean matches(InventoryCrafting par1InventoryCrafting,
 			World par2World) {
 		
-		for (int i = 0; i <= 3 - this.recipeWidth; ++i) {
-			for (int j = 0; j <= 3 - this.recipeHeight; ++j) {
-				if (this.checkMatch(par1InventoryCrafting, i, j, true)) {
-					return true;
-				}
-
-				if (this.checkMatch(par1InventoryCrafting, i, j, false)) {
-					return true;
-				}
-			}
+		if(par1InventoryCrafting.getStackInSlot(0) == null) {
+			return false;
 		}
-
-		return false;
-	}
-
-	/**
-	 * Checks if the region of a crafting inventory is match for the recipe.
-	 */
-	private boolean checkMatch(InventoryCrafting par1InventoryCrafting,
-			int par2, int par3, boolean par4) {
 		
-		for (int k = 0; k < 3; ++k) {
-			for (int l = 0; l < 3; ++l) {
-				int i1 = k - par2;
-				int j1 = l - par3;
-				ItemStack itemstack = null;
-
-				if (i1 >= 0 && j1 >= 0 && i1 < this.recipeWidth
-						&& j1 < this.recipeHeight) {
-					if (par4) {
-						itemstack = this.recipeItems[this.recipeWidth - i1 - 1
-								+ j1 * this.recipeWidth];
-					} else {
-						itemstack = this.recipeItems[i1 + j1 * this.recipeWidth];
-					}
-				}
-
-				ItemStack itemstack1 = par1InventoryCrafting
-						.getStackInRowAndColumn(k, l);
-
-				if (itemstack1 != null || itemstack != null) {
-					if (itemstack1 == null && itemstack != null
-							|| itemstack1 != null && itemstack == null) {
-						return false;
-					}
-
-					if (itemstack.itemID != itemstack1.itemID) {
-						return false;
-					}
-
-//					if (itemstack.getItemDamage() != 32767
-//							&& itemstack.getItemDamage() != itemstack1
-//									.getItemDamage()) {
-//						return false;
-//					}
-				}
-			}
+		if(par1InventoryCrafting.getStackInSlot(1) == null) {
+			return false;
 		}
-
-		return true;
+		
+		if(par1InventoryCrafting.getStackInSlot(2) == null) {
+			return false;
+		}
+		
+		return true;	
 	}
 
 	@Override
