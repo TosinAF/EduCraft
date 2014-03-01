05e5fe1161b8eccec3a34311a977b6bfad4c14bb
Janos Bana <janosbana@gmail.com>
2014-03-01 21:04:10 +0000

    adds textures to numbers based on itemDamage value, changes texture of operators

--- mcp/src/minecraft/org/educraft/number/BaseNumber.java
+++ mcp/src/minecraft/org/educraft/number/BaseNumber.java
@@ -2,10 +2,14 @@ package org.educraft.number;
 
 import org.educraft.EduCraft;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 
 public class BaseNumber extends Item {
 
@@ -13,7 +17,6 @@ public class BaseNumber extends Item {
 		super(itemID);
 		setHasSubtypes(true);
 		setMaxDamage(0);
-		setTextureName("diamond");
 		setMaxStackSize(1); // Change this later perhaps
 		setCreativeTab(EduCraft.tabEduCraft);
 		setUnlocalizedName("number");
@@ -22,5 +25,25 @@ public class BaseNumber extends Item {
 	@Override
 	public String getUnlocalizedName(ItemStack is) {
 		return getUnlocalizedName() + "." + String.valueOf(is.getItemDamage());
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static Icon[] icons;
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIconFromDamage(int damage) {
+		return icons[damage];
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister icon) {
+		icons = new Icon[EduCraft.MAX_NUMBER];
+		
+		for(int i = 1; i < EduCraft.MAX_NUMBER; i++) {
+			
+			icons[i] = icon.registerIcon("educraft:" + i);
+		}
 	}
 }
