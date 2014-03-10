7285a28fd046a4e80a3ce6ca9716752ed9b26bb3
Janos Bana <janosbana@gmail.com>
2014-03-10 21:28:15 +0000

    changes textures for custom blocks

--- mcp/src/minecraft/org/educraft/block/operators/BlockOperatorBench.java
+++ mcp/src/minecraft/org/educraft/block/operators/BlockOperatorBench.java
@@ -2,8 +2,13 @@ package org.educraft.block.operators;
 
 import org.educraft.EduCraft;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockWorkbench;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
 public class BlockOperatorBench extends BlockWorkbench {
@@ -11,6 +16,11 @@ public class BlockOperatorBench extends BlockWorkbench {
 	 * The id used to identify this block in the GuiHandler.
 	 */
 	public static final int GUI_ID = 1;
+	
+	@SideOnly(Side.CLIENT)
+	private Icon operatorIconTop;
+	@SideOnly(Side.CLIENT)
+	private Icon operatorIconFront;
 
 	public BlockOperatorBench(int id) {
 		super(id);
@@ -18,6 +28,23 @@ public class BlockOperatorBench extends BlockWorkbench {
 		setCreativeTab(EduCraft.tabEduCraft);
 	}
 
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int par1, int par2) {
+		return par1 == 1 ? this.operatorIconTop : (par1 == 0 ? Block.planks
+				.getBlockTextureFromSide(par1)
+				: (par1 != 2 && par1 != 4 ? this.blockIcon
+						: this.operatorIconFront));
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister register) {
+		this.blockIcon = register.registerIcon("EduCraft:operator_bench_side");
+		this.operatorIconTop = register
+				.registerIcon("EduCraft:operator_bench_top");
+		this.operatorIconFront = register
+				.registerIcon("EduCraft:operator_bench_front");
+	}
+	
 	@Override
 	public boolean onBlockActivated(World par1World, int par2, int par3,
 			int par4, EntityPlayer par5EntityPlayer, int par6, float par7,
@@ -30,5 +57,5 @@ public class BlockOperatorBench extends BlockWorkbench {
 			return false;
 		}
 	}
-
+	
 }
--- mcp/src/minecraft/org/educraft/block/ordering/BlockOrderingBench.java
+++ mcp/src/minecraft/org/educraft/block/ordering/BlockOrderingBench.java
@@ -3,21 +3,48 @@ package org.educraft.block.ordering;
 import org.educraft.EduCraft;
 import org.educraft.number.block.BlockTileEntity;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockWorkbench;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
 public class BlockOrderingBench extends BlockContainer {
 
 	public static final int GUI_ID = 2;
 	
+	@SideOnly(Side.CLIENT)
+	private Icon orderingIconTop;
+	@SideOnly(Side.CLIENT)
+	private Icon orderingIconFront;
+	
 	public BlockOrderingBench(int id) {
 		super(id, Material.ground);
 		setUnlocalizedName("Ordering Bench");
 		setCreativeTab(EduCraft.tabEduCraft);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int par1, int par2) {
+		return par1 == 1 ? this.orderingIconTop : (par1 == 0 ? Block.planks
+				.getBlockTextureFromSide(par1)
+				: (par1 != 2 && par1 != 4 ? this.blockIcon
+						: this.orderingIconFront));
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister register) {
+		this.blockIcon = register.registerIcon("EduCraft:ordering_bench_side");
+		this.orderingIconTop = register
+				.registerIcon("EduCraft:ordering_bench_top");
+		this.orderingIconFront = register
+				.registerIcon("EduCraft:ordering_bench_front");
 	}
 	
 	@Override
