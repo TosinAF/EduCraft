cab7fa8da4c82f4ed30866c6c1fcce926e17bcfa
BerkshireKnight <ian.knight.1990@gmail.com>
2014-03-19 14:44:04 +0000

    Added extra files to make odd/even bench more clear

---
+++ mcp/src/minecraft/org/educraft/block/ordering/BenchType.java
@@ -0,0 +1,29 @@
+package org.educraft.block.ordering;
+
+/**
+ * Enum type describing the different kinds of ordering bench.
+ */
+public enum BenchType {
+	/**
+	 * A bench which accepts all numbers.
+	 */
+	ALL(0),
+	/**
+	 * A bench which accepts only odd numbers.
+	 */
+	ODD(1),
+	/**
+	 * A bench which accepts only even numbers.
+	 */
+	EVEN(2);
+	
+	private int type;
+	
+	BenchType(int type) {
+		this.type = type;
+	}
+	
+	public int getTypeCode() {
+		return this.type;
+	}
+}
