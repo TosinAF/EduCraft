6b09ad8ada5cf22962e0128bef21dd6b66093fa0
BerkshireKnight <ian.knight.1990@gmail.com>
2014-03-25 13:31:38 +0000

    Fixes #29

--- mcp/src/minecraft/org/educraft/block/calculator/CalculatorContainer.java
+++ mcp/src/minecraft/org/educraft/block/calculator/CalculatorContainer.java
@@ -118,50 +118,42 @@ public class CalculatorContainer extends Container {
 	 * 
 	 * @param player
 	 *            the player doing the shift-clicking
-	 * @param par2
-	 *            unknown
+	 * @param i
+	 *            index of the slot that was shift-clicked
 	 * @return the results of the shift click
 	 */
 	@Override
-	public ItemStack transferStackInSlot(EntityPlayer player, int par2) {
-		ItemStack itemstack = null;
-		Slot slot = (Slot) this.inventorySlots.get(par2);
+	public ItemStack transferStackInSlot(EntityPlayer player, int i) {
+		ItemStack tempStack = null;
+		Slot slot = (Slot) this.inventorySlots.get(i);
 
 		if (slot != null && slot.getHasStack()) {
-			ItemStack itemstack1 = slot.getStack();
-			itemstack = itemstack1.copy();
+			ItemStack slotStack = slot.getStack();
+			tempStack = slotStack.copy();
 
-			if (par2 == 0) {
-				if (!this.mergeItemStack(itemstack1, 10, 46, true)) {
+			if (i < 4) {
+				// item was in container, place in player's inventory
+				if (!this.mergeItemStack(slotStack, 4, 35, true)) {
 					return null;
 				}
-
-				slot.onSlotChange(itemstack1, itemstack);
-			} else if (par2 >= 10 && par2 < 37) {
-				if (!this.mergeItemStack(itemstack1, 37, 46, false)) {
-					return null;
-				}
-			} else if (par2 >= 37 && par2 < 46) {
-				if (!this.mergeItemStack(itemstack1, 10, 37, false)) {
-					return null;
-				}
-			} else if (!this.mergeItemStack(itemstack1, 10, 46, false)) {
+			} else if (!this.mergeItemStack(slotStack, 1, 4, false)) {
+				// item was in inventory, place in container
 				return null;
 			}
 
-			if (itemstack1.stackSize == 0) {
+			// update the slot appropriately
+			if (slotStack.stackSize == 0) {
 				slot.putStack((ItemStack) null);
 			} else {
 				slot.onSlotChanged();
 			}
 
-			if (itemstack1.stackSize == itemstack.stackSize) {
+			if (slotStack.stackSize == tempStack.stackSize) {
 				return null;
 			}
-
-			slot.onPickupFromSlot(player, itemstack1);
+			slot.onPickupFromSlot(player, slotStack);
 		}
 
-		return itemstack;
+		return tempStack;
 	}
 }
