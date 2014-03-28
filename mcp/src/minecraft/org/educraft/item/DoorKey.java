30a532885985d5b64e8e34e9cf34912a3d936870
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-03-28 20:36:12 +0000

    added docs

--- mcp/src/minecraft/org/educraft/item/DoorKey.java
+++ mcp/src/minecraft/org/educraft/item/DoorKey.java
@@ -15,6 +15,13 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class DoorKey extends Item {
 	public static final String[] NAMES = { "key_red", "key_blue", "key_yellow" };
+	
+	/**
+	 * The Door Key Constructor
+	 * 
+	 * @param itemID
+	 *           item id
+	 */
 
 	public DoorKey(int itemId) {
 		super(itemId);
@@ -28,12 +35,23 @@ public class DoorKey extends Item {
 		setMaxStackSize(4);
 		setCreativeTab(EduCraft.tabEduCraft);
 	}
+	
+	/**
+	 * Returns a string of the item's name & it's damage value
+	 * 
+	 * @param is
+	 *           An Item Stack
+	 */
 
 	@Override
 	public String getUnlocalizedName(ItemStack is) {
 		return getUnlocalizedName() + "." + NAMES[is.getItemDamage()];
 	}
-
+	
+	/**
+	 * Returns the children items of the parent item
+	 */
+	 
 	@Override
 	public void getSubItems(int id, CreativeTabs creativeTab, List list) {
 		for (int i = 0; i < NAMES.length; i++) {
@@ -41,15 +59,33 @@ public class DoorKey extends Item {
 			list.add(stack);
 		}
 	}
+	
+	/**
+	 * Array where every index will contain an icon for each damage level
+	 */
 
 	@SideOnly(Side.CLIENT)
 	public static Icon[] icons;
+	
+	/**
+	 * Returns the icon for a specified damage level
+	 * 
+	 * @param damage
+	 *           level of damage of the item
+	 */
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public Icon getIconFromDamage(int damage) {
 		return icons[damage];
 	}
+	
+	/**
+	 * Populates the icon array with the approitate icons for each damage level
+	 * 
+	 * @param damage
+	 *           level of damage of the item
+	 */
 
 	@Override
 	@SideOnly(Side.CLIENT)
