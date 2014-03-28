6b6d16535ed9e37d4e0fb6bcc70628778874d6df
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-03-28 20:32:24 +0000

    added docs

--- mcp/src/minecraft/org/educraft/item/OperatorType.java
+++ mcp/src/minecraft/org/educraft/item/OperatorType.java
@@ -3,17 +3,30 @@ package org.educraft.item;
 public enum OperatorType {
 	TIMES("Multiplication", "x"), DIVIDE("Division", "/"), PLUS("Addition", "+"), MINUS(
 			"Subtraction", "-");
+			
 
 	private String name, symbol;
+	
+	/**
+	 * The Operator Type Enum Constructor
+	 */
 
 	private OperatorType(String name, String symbol) {
 		this.name = name;
 		this.symbol = symbol;
 	}
+	
+	/**
+	 * Returns the operator name
+	 */
 
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Returns the operator symbol
+	 */
 
 	public String getSymbol() {
 		return symbol;
